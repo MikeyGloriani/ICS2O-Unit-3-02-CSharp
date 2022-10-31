f428860963ef21d4851fb29c2e392fb839d2d9c6
MikeyGloriani <113134835+MikeyGloriani@users.noreply.github.com>
2022-10-31 14:32:11 -0400

    init

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# ICS2O-Unit2-01-CSharp
+# ICS2O-Unit-3-02-CSharp
 
-[![Mr Coxall's Super Linter](https://github.com/MikeyGloriani/ICS2O-Unit2-01-CSharp/workflows/Mr%20Coxall's%20Super%20Linter/badge.svg)](https://github.com/MikeyGloriani/ICS2O-Unit2-01-CSharp/actions/)
+[![Mr Coxall's Super Linter](https://github.com/MikeyGloriani/ICS2O-Unit-3-02-CSharp/workflows/Mr%20Coxall's%20Super%20Linter/badge.svg)](https://github.com/MikeyGloriani/ICS2O-Unit-3-02-CSharp/actions/)
 
-[![Run on Repl.it](https://repl.it/badge/github/MikeyGloriani/ICS2O-ICS2O-Unit2-01-CSharp)](https://repl.it/github/MikeyGloriani/ICS2O-Unit2-01-CSharp)
+[![Run on Repl.it](https://repl.it/badge/github/MikeyGloriani/ICS2O-Unit-3-02-CSharp)](https://repl.it/github/MikeyGloriani/ICS2O-Unit-3-02-CSharp)
--- main.cs
+++ main.cs
@@ -1,16 +1,38 @@
 // Created by: Mikey Gloriani
 // Created on: Oct 2022
 //
-// This program displays, "Hello, World!"
+// This program displays the volume of a pyramid
 
 
 using System;
 
-class Program
+class Program 
 {
-    public static void Main(string[] args)
+    public static void Main (string[] args) 
     {
-        Console.WriteLine("Hello, World!");
+        // This function accepts user input
+        int length;
+        int width;
+        int height;
+        int area;
+
+        Console.WriteLine("This program displays the volume of a pyramid.");
+        Console.WriteLine("");
+
+        Console.Write("Enter the length (cm): ");
+        length = Convert.ToInt32(Console.ReadLine());
+        
+        Console.Write("Enter the width (cm): ");
+        width = Convert.ToInt32(Console.ReadLine());
+        
+        Console.Write("Enter the height (cm): ");
+        height = Convert.ToInt32(Console.ReadLine());
+
+        area = ((length*width*height)/3);
+
+        Console.WriteLine("");
+        Console.WriteLine("The volume is: " + area + " cm³. ");
+
         Console.WriteLine("\nDone.");
     }
 }
