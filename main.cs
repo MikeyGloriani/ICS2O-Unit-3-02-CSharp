6035577722c213bc571cec1f6c0ba97e50328103
MikeyGloriani <113134835+MikeyGloriani@users.noreply.github.com>
2022-10-31 14:38:27 -0400

    init

--- main.cs
+++ main.cs
@@ -5,7 +5,6 @@
 
 
 using System;
-
 class Program 
 {
     public static void Main (string[] args) 
@@ -15,24 +14,17 @@ class Program
         int width;
         int height;
         int area;
-
         Console.WriteLine("This program displays the volume of a pyramid.");
         Console.WriteLine("");
-
         Console.Write("Enter the length (cm): ");
         length = Convert.ToInt32(Console.ReadLine());
-        
         Console.Write("Enter the width (cm): ");
         width = Convert.ToInt32(Console.ReadLine());
-        
         Console.Write("Enter the height (cm): ");
         height = Convert.ToInt32(Console.ReadLine());
-
         area = ((length*width*height)/3);
-
         Console.WriteLine("");
         Console.WriteLine("The volume is: " + area + " cm³. ");
-
         Console.WriteLine("\nDone.");
     }
 }
